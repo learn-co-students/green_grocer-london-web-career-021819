60c5cbc9acc5b1f61890ca547d9319817c6dbca0
Calie Rushton <calielrushton@gmail.com>
2019-01-29 11:02:22 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -16,18 +16,48 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  coup = coupons[0][:item]
-  cost = coupons[0][:cost]
-    if cart.has_key?(coup)
-      cart["#{coup} W/COUPON"] = {:price => cost}
+  coupons.each do |coupon|
+    grocery = coupon[:item]
+    if cart.has_key?(grocery) && cart[grocery][:count] >= coupon[:num]
+      if cart["#{grocery} W/COUPON"] # if this entry exists, you've already applied a coupon, so...
+        # apply the next one, increment count, decrement number of items left in cart 'uncoupened'
+        cart["#{grocery} W/COUPON"][:count] += 1
+        cart[grocery][:count] -= coupon[:num]
+      else
+        # set the 'couponed' entry with count of 1...
+      cart["#{grocery} W/COUPON"] = {:price => coupon[:cost], :count => 1, :clearance => cart[grocery][:clearance]}
+      cart[grocery][:count] -= coupon[:num] # decrement uncoupened items left in cart
+      end
     end
+  end
   cart
 end
 
-def apply_clearance(cart)
-  # code here
+
+def apply_clearance(cart) # not an array in this test case!
+  cart.each do |grocery, values|
+    if values[:clearance]
+      values[:price] = (values[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidated = consolidate_cart(cart)
+  couponed = apply_coupons(consolidated, coupons)
+  cleared = apply_clearance(couponed)
+
+  # calculate the total
+  total = 0
+  cleared.each do |item, attributes|
+    total += attributes[:price]
+  end
+  total
+
+  # if total > 100
+  #   to_pay = total * 0.1
+  # end
+  # to_pay
+
 end
