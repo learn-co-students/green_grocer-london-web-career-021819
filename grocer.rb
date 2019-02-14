228b2fc1aa0e1e8a9641f018cbd60ea56a19d192
Ali Madni <ali555m@hotmail.com>
2019-02-14 14:24:49 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -42,7 +42,15 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
+  total = 0 #running total
   cart = consolidate_cart(cart)
   cart = apply_coupons(cart, coupons)
-  cart = apply_clearance(cart)
+  cart = apply_clearance(cart) #applying various discounts
+  cart.each do |items, item_name|
+    total = total + item_name[:price] * item_name[:count] #total is the current total + price of each itme * the number of each item
+  end
+  if total > 100
+    total = (total * 0.9).round(2) #10% off if over 100
+  end
+  total
 end
