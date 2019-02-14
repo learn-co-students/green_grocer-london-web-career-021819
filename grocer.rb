33cc804fe51839b4f594d9f5e6e439a1196662b7
Ali Madni <ali555m@hotmail.com>
2019-02-14 14:09:20 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -14,12 +14,12 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  coupons.each do |info|
-    c_item = info[:item]
-    c_num = info[:num]
-    c_cost = info[:cost]
-    c_name = "#{c_item} W/COUPON"
-    if cart[c_item] && cart[c_item][:count] >= c_num
+  coupons.each do |info| #iterating over the info in the coupons hash
+    c_item = info[:item] #name of the item in the coupon
+    c_num = info[:num] #number of items for the coupon to work
+    c_cost = info[:cost]  #price of the items after teh coupon
+    c_name = "#{c_item} W/COUPON" # name of couponed items in the cart
+    if cart[c_item] && cart[c_item][:count] >= c_num #if the cart contains the coupon items in the right amount
       num = cart[c_item][:count] / c_num
       cart[c_name] = {price: c_cost, clearance: cart[c_item][:clearance], count: num}
 
